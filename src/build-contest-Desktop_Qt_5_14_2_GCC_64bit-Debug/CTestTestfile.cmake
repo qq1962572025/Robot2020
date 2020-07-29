60707dfa8dc734c99094aa93f2aa1721257e3d89
jadeite_fantasy <1962572025@qq.com>
2020-07-29 14:36:03 +0800

    200729_3

--- src/build-contest-Desktop_Qt_5_14_2_GCC_64bit-Debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/slam/Robot2020/src/contest
-# Build directory: /home/slam/Robot2020/src/build-contest-Desktop_Qt_5_14_2_GCC_64bit-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- src/build-contest-Desktop_Qt_5_14_2_GCC_64bit-Debug/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/slam/Robot2020/src/build-contest-Desktop_Qt_5_14_2_GCC_64bit-Debug/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
